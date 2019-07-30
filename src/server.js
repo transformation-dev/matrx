9653f693d193cc5f7adf54982151ff79af11ce54
Larry Maccherone <Larry@Maccherone.com>
2019-07-30 16:45:31 -0400

    Faye installed. Pushing to confirm works in Azure

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+// import polka from 'polka'  // TODO: npm uninstall --save polka
+import express from 'express'
+import compression from 'compression'
+import * as sapper from '@sapper/server'
+import faye from 'faye'
+import http from 'http'
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const { PORT, NODE_ENV } = process.env
+const dev = NODE_ENV === 'development'
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = express()
+
+const server = http.createServer(app)
+
+const pubsub = new faye.NodeAdapter({mount: '/pubsub', timeout: 45})
+pubsub.attach(server)
+
+app.use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+)
+
+server.listen(PORT, err => {
+	if (err) console.log('error', err);
+})
