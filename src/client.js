02c35e4b9f2c95412478e723ea84fc29ba130685
Larry Maccherone <Larry@Maccherone.com>
2019-08-13 14:45:01 -0400

    Bootstrap css and js all working from npm

--- src/client.js
+++ src/client.js
@@ -1,5 +1,14 @@
 import * as sapper from '@sapper/app'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'popper.js'  // Popovers seem to work fine without this import. Maybe it's imported automatically with bootstrap
+import $ from 'jquery'
+import 'bootstrap'
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 sapper.start({
 	target: document.querySelector('#sapper')
 })
