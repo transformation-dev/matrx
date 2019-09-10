8c976ad7733fbe1574b6c651bcabea2943eb648e
Larry Maccherone <Larry@Maccherone.com>
2019-09-09 20:10:18 -0400

    Cleaned up poc poc2

--- src/routes/poc/index.svelte
+++ src/routes/poc/index.svelte
@@ -1,94 +1,36 @@
 <script>
   import { writable } from 'svelte/store'
   import {onMount} from 'svelte'
-  import { stores } from '@sapper/app'
-  const { preloading, page, session } = stores()
 
   import urlComposer from 'url-composer'
-  
-  import {getClient} from '@matrx/realtime'
-
-  const a = writable(0)
-  const b = writable(10)
-  const pageStores = {a, b}
-  let pubsubClient 
+	
+	import io from 'socket.io-client'
+	
+  // import {getClient} from '@matrx/realtime'
+	// let pubsubClient 
+	
+	let socket
 
   onMount(() => {
-    pubsubClient = getClient()
-    subscribe()
+		// pubsubClient = getClient()
+		socket = io()
+		socket.on('connect', function(e){
+			console.log('connected', e)
+		})
+  	socket.on('event', function(data){
+			console.log('web socket event', data)
+		})
+  	socket.on('disconnect', function(e){
+			console.log('disconnected', e)
+		})
 	})
-
-  function subscribe() {
-  	const url = urlComposer.build({
-  		path: 'pubsub/subscribe', 
-  	})
-  	fetch(url, { 
-  		method: 'POST',
-  		body: JSON.stringify({
-        connectionID: pubsubClient.connectionID,
-        type: 'syncable',
-        // TODO: When we move this into the store, we'll be passing in a app-wide uique string for this store
-        channelID: 'poc.a',  // TODO: Find some way to autopopulate this on build. Might require writing a rollup plugin maybe with this: https://github.com/jetiny/rollup-plugin-re
-      }),
-  		headers:{
-  			'Content-Type': 'application/json',
-  		},
-    }).then((res) => {
-      console.log('got response from handleSubscribe')
-      res.text().then((text) => {
-        console.log(text)
-      })
-    })
-  }
   
   function handleMessage(event) {
-  	const url = urlComposer.build({
-  		path: 'pubsub/send-message/:connectionID', 
-  		params: {connectionID: pubsubClient.connectionID},
-  	})
-  	const res = fetch(url, { 
-  		method: 'POST',
-  		body: JSON.stringify({
-        say: 'something'
-      }),
-  		headers:{
-  			'Content-Type': 'application/json',
-  		},
-    })
+		console.log('inside handleMessage')
+  	socket.emit('my broadcast', {say: 'something'})
   }
   
-  function handlePublish(event) {
-   	const url = urlComposer.build({
-   		path: 'pubsub/publish/:channelID', 
-   		params: {channelID: 'poc.a'},
-   	})
-   	const res = fetch(url, {
-   		method: 'POST',
-   		body: JSON.stringify({msg: 'from publish'}),
-   		headers:{
-   			'Content-Type': 'application/json',
-   		},
-   	})
-  }
-
-  function handlePublishFrom(event) {
-   	const url = urlComposer.build({
-   		path: 'pubsub/publish/:channelID/from/:connectionID', 
-   		params: {channelID: 'poc.a', connectionID: pubsubClient.connectionID},
-   	})
-   	const res = fetch(url, {
-   		method: 'POST',
-   		body: JSON.stringify({msg: 'from publish'}),
-   		headers:{
-   			'Content-Type': 'application/json',
-   		},
-   	})
-  }
 
 </script>
 
-<h1>Hello {$a} {$b}</h1>
-
 <button on:click={handleMessage} class="button">Message</button>
-<button on:click={handlePublish} class="button">Publish</button>
-<button on:click={handlePublishFrom} class="button">Publish From</button>
