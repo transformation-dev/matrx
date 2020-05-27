ba8d6bcedb405ef6cd3d52a0f9e8b20349e1d527
lmaccherone <larry@maccherone.com>
2020-05-27 15:53:28 -0400

    Removed all references to SortableList

--- src/routes/Morgan.svelte
+++ src/routes/Morgan.svelte
@@ -1,10 +1,6 @@
 <script>
-import SortableList from '../SortableList'
+  console.log('nothing here')
 
-let list = ["First Item", "Second Item", "Third Item"]
-const sortList = ev => {
-  list = ev.detail
-}
 </script>
 
 <h1> Hey, it's Morgan! </h1>
@@ -15,26 +11,3 @@ const sortList = ev => {
   </div>
 </div>
 
-<div class="columns is-gapless">
-  <div class="column is-one-third has-text-centered">
-    First column
-    <SortableList 
-        {list} 
-        on:sort={sortList}
-    />
-  </div>
-  <div class="column is-one-third has-text-centered">
-    Second column
-    <SortableList 
-      {list} 
-      on:sort={sortList}
-    />
-  </div>
-  <div class="column is-one-third has-text-centered">
-    Third column
-    <SortableList 
-      {list} 
-      on:sort={sortList}
-    />
-  </div>
-</div>
