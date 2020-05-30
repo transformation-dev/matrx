e0ec32aef423dfa4ad627d1a2d937efe2dc804c9
lmaccherone <larry@maccherone.com>
2020-05-29 23:59:56 -0400

    coverage in the high 80's but still failing drop on DoingKanban

--- cypress/integration/plan.spec.js
+++ cypress/integration/plan.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="Cypress" />
 
 context('Plan', () => {
@@ -29,7 +30,23 @@ context('Plan', () => {
     
     cy.get('#pan-right')
       .contains("Doing")
+      .trigger('dragenter')
+
+    cy.get('#pan-right')
+      .should('have.class', 'has-background-grey-lighter')
+
+    cy.get('#pan-right')
+      .trigger('dragleave')
+
+    cy.get('#pan-right')
+      .should('not.have.class', 'has-background-grey-lighter')
+
+    cy.get('#pan-right')
+      .trigger('dragenter')
       .trigger('drop')
+
+    cy.get('#pan-right')
+      .should('not.have.class', 'has-background-grey-lighter')
       .click()
 
     cy.should(() => {
@@ -44,6 +61,41 @@ context('Plan', () => {
 
     cy.get('#practice5')
       .contains("Network Originated Scans")
+
+    cy.visit('localhost:8080/#/test-jig')
+    cy.wait(2000)
+    cy.url().should('include', 'teamID=team1')
+    cy.url().should('not.include', 'startOn')
+    cy.url().should('not.include', 'slidesToDisplay')
+
+    cy.visit('localhost:8080/#/plan')
+    cy.wait(2000)
+    cy.url().should('include', 'teamID=team1')
+    cy.url().should('include', 'startOn=1')
+    cy.url().should('include', 'slidesToDisplay=1')
+
+    cy.get('#practice5')
+      .trigger('dragstart')
+    
+    cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+      .trigger('dragenter')
+
+    cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+      .should('have.class', 'has-background-grey-lighter')
+
+    cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+      .trigger('dragleave')
+
+    cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+      .should('not.have.class', 'has-background-grey-lighter')
+      
+    // cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+    //   .trigger('dragenter')
+    //   .trigger('drop')
+
+    // cy.get(`#${CSS.escape('(queue1, Actions)')}`)
+    //   .contains("Network Originated Scans")
+      
   })
 
   // eslint-disable-next-line no-undef
--- packages/svelte-viewstate-store/svelte-viewstate-store.js
+++ packages/svelte-viewstate-store/svelte-viewstate-store.js
@@ -15,7 +15,7 @@ export class ViewstateStore {
     this.subscribers = []
     this.scope = storeConfig.scope
 
-    this._set(storeConfig.defaultValue)  // This is just so value is not undefined. It gets updated by the querystring or LocalStorage 
+    // this._set(storeConfig.defaultValue)  // This is just so value is not undefined. It gets updated by the querystring or LocalStorage 
 
     this.onURLChange = this.onURLChange.bind(this)
     const unsubscribe = loc.subscribe(this.onURLChange)
@@ -40,10 +40,11 @@ export class ViewstateStore {
     return {newValue, urlSearchParams}
   }
 
-  setQueryStringParam(urlSearchParams, newValue) {
-    urlSearchParams.set(this.storeConfig.identifier, newValue)
-    push(this.location + '?' + urlSearchParams.toString())
-  }
+  // setQueryStringParam(urlSearchParams, newValue) {
+  //   ViewstateStore.queueURLUpdate(this.storeConfig.identifier, newValue)
+  //   // urlSearchParams.set(this.storeConfig.identifier, newValue)
+  //   // push(this.location + '?' + urlSearchParams.toString())
+  // }
 
   onURLChange(newLoc) {
     this.scope = this.scope || newLoc.location
@@ -62,7 +63,8 @@ export class ViewstateStore {
       }
     } else {
       newValue = window.localStorage[this.scope + '.' + this.storeConfig.identifier] || this.storeConfig.defaultValue
-      this.setQueryStringParam(urlSearchParams, newValue)
+      ViewstateStore.queueURLUpdate(this.storeConfig.identifier, newValue)
+      // this.setQueryStringParam(urlSearchParams, newValue)
     }
     this._set(newValue)
   }
@@ -71,7 +73,8 @@ export class ViewstateStore {
     this._set(newValue)
     window.localStorage[this.scope + '.' + this.storeConfig.identifier] = newValue
     const {urlSearchParams} = this.getQuerystringParam()
-    this.setQueryStringParam(urlSearchParams, newValue)
+    ViewstateStore.queueURLUpdate(this.storeConfig.identifier, newValue)
+    // this.setQueryStringParam(urlSearchParams, newValue)
   }
   
   _set(newValue) {  // Call this to update subscribers when the URL changes w/o saving to LocalStorage
@@ -102,7 +105,7 @@ export class ViewstateStore {
     const subscriber = [run, invalidate]
     this.subscribers.push(subscriber)
     if (this.subscribers.length === 1) {
-      this.stop = this.start(this.set) || noop  // TODO: Should this be this._set
+      this.stop = this.start(this._set) || noop  // TODO: Should this be this._set
     }
     run(this.value)
 
@@ -120,4 +123,45 @@ export class ViewstateStore {
     return this.unsubscribe
   }
 
+}
+
+function getLocation() {
+  const hashPosition = window.location.href.indexOf('#/')
+  let location = (hashPosition > -1) ? window.location.href.substr(hashPosition + 1) : '/'
+  // Check if there's a querystring
+  const qsPosition = location.indexOf('?')
+  let querystring = ''
+  if (qsPosition > -1) {
+    querystring = location.substr(qsPosition + 1)
+    location = location.substr(0, qsPosition)
+  }
+  const urlSearchParams = new URLSearchParams(querystring)
+  return {urlSearchParams, location, querystring}
+}
+
+ViewstateStore.pendingURLUpdates = {}
+ViewstateStore.timer = null
+
+ViewstateStore.queueURLUpdate = (key, value) => {
+  if (ViewstateStore.timer) {
+    clearTimeout(ViewstateStore.timer)
+    ViewstateStore.timer = null
+  }
+  ViewstateStore.pendingURLUpdates[key] = value
+  ViewstateStore.timer = setTimeout(ViewstateStore.processPendingURLUpdates, 0)
+}
+
+ViewstateStore.processPendingURLUpdates = function() {
+  const {urlSearchParams, location} = getLocation()
+  const currentURLSearchString = urlSearchParams.toString()
+  console.log('location inside processPendingURLUpdates', location, urlSearchParams.toString())
+  for (const [key, value] of Object.entries(ViewstateStore.pendingURLUpdates)) {
+    urlSearchParams.set(key, value)
+  }
+  const newURLSearchString = urlSearchParams.toString()
+  if (newURLSearchString !== currentURLSearchString) {
+    push(location + '?' + newURLSearchString)
+  }
+  ViewstateStore.pendingURLUpdates = {}
+  ViewstateStore.timer = null
 }
