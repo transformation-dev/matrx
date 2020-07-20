396dbdc0c810ebd96c7dc1553cced9884664f7fb
lmaccherone <larry@maccherone.com>
2020-07-20 11:14:52 -0400

    moved matrx.scss compiliation into webpack

--- src/routes/Poc.svelte
+++ src/routes/Poc.svelte
@@ -6,8 +6,10 @@
 <div style="height: 0.5rem" />
 
 <h1>POC</h1>
-
+<div class="rotate-left">Something to test styles</div>
 
 <style>
-
+  .rotate-left {
+    transform: rotate(-90deg);
+  }
 </style>
