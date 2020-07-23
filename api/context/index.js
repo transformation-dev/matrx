a7950627861926bee11f2287be8ab153e588c0cb
lmaccherone <larry@maccherone.com>
2020-07-23 13:16:35 -0400

    Pushing again to figure out why local behaves differently

--- api/context/index.js
+++ api/context/index.js
@@ -1,12 +1,12 @@
 module.exports = async function (context, req) {
   const serverEnvironment = process.env.NODE_ENV
-  
+
   context.res = {
     // status: 200, /* Defaults to 200 */
     headers: {
       'Content-Type': 'application/json'
     },
-    body: {serverEnvironment}
+    body: {nodeEnv: process.env.NODE_ENV, serverEnvironment}
   }
 
   // const name = req.query.name || (req.body && req.body.name)
